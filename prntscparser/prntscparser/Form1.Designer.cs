33a16276d3778976d246c22da3020d6b89626f5b
Muphlon <37045527+Markus1k@users.noreply.github.com>
2020-03-16 22:26:45 +0200

    Upload

---
+++ prntscparser/prntscparser/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace prntscparser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.ByMuphlonLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Prepic = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Prepic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(298, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Parse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "path for saving images for prnt.sc ->";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(298, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Open Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(383, 80);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // ByMuphlonLabel
+            // 
+            this.ByMuphlonLabel.AutoSize = true;
+            this.ByMuphlonLabel.Location = new System.Drawing.Point(376, 279);
+            this.ByMuphlonLabel.Name = "ByMuphlonLabel";
+            this.ByMuphlonLabel.Size = new System.Drawing.Size(63, 13);
+            this.ByMuphlonLabel.TabIndex = 13;
+            this.ByMuphlonLabel.Text = "By Muphlon";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Parse quantity";
+            // 
+            // Prepic
+            // 
+            this.Prepic.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Prepic.Location = new System.Drawing.Point(27, 80);
+            this.Prepic.Name = "Prepic";
+            this.Prepic.Size = new System.Drawing.Size(257, 198);
+            this.Prepic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Prepic.TabIndex = 15;
+            this.Prepic.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(129, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 301);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Prepic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ByMuphlonLabel);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "prntsc parser 2.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Prepic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label ByMuphlonLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox Prepic;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ prntscparser/prntscparser/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+namespace prntscparser
+{
+    public partial class Form1 : Form
+    {
+        int repeat_times; //how many times program do parse
+        IWebDriver phjs;
+        string location;
+        public static int times = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e) //"Parse" click
+        {
+            Thread th = new Thread(parse);
+            th.Start();
+        }
+ 
+        public void parse()
+        {
+
+            int name = 0;
+            phjs = new PhantomJSDriver();
+            times = Decimal.ToInt32(numericUpDown1.Value); // how many times program must do parse
+            for (repeat_times = 0; repeat_times < times; repeat_times++)
+            {
+                string words = "qwertyuiopasdfghjklzxcvbnm1234567890";
+                int lng = words.Length;
+                string result = "";
+                int char_in_url = 6;
+                Random rnd = new Random();
+                for (int i = 0; i < char_in_url; i++)
+                {
+                    result += words[rnd.Next(lng)].ToString();
+                }
+             
+                phjs.Navigate().GoToUrl("https://prnt.sc/" + result);
+                name++;
+                Prepic.Image = ByteToImage((phjs as PhantomJSDriver).GetScreenshot().AsByteArray);
+                (phjs as PhantomJSDriver).GetScreenshot().SaveAsFile(location + "\\"  + name +".png", System.Drawing.Imaging.ImageFormat.Png);
+               System.Threading.Thread.Sleep(1000);
+
+            }
+            if (repeat_times == times)
+            {
+               DialogResult dg = MessageBox.Show("Done!","Prnt.sc Parser",MessageBoxButtons.YesNo);
+                if(dg == DialogResult.Yes)
+                {
+
+                    Application.ExitThread();
+
+                }
+                else
+                {
+                    Application.ExitThread();
+                }
+            }
+        }
+        public static Bitmap ByteToImage(byte[] blob) //From stuckoverflow :D
+        {
+            MemoryStream mStream = new MemoryStream();
+            byte[] pData = blob;
+            mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+            Bitmap bm = new Bitmap(mStream, false);
+            mStream.Dispose();
+            return bm;
+        }
+
+        private void button2_Click(object sender, EventArgs e) // "Path" click
+        {
+            var fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+               
+            if (result == DialogResult.OK)
+            {
+                location = fbd.SelectedPath;
+                Console.WriteLine(location);
+                label1.Text = location;
+                var st_writer = new StreamWriter("path.txt");
+                st_writer.Write(location);
+                st_writer.Close();
+            }
+        }
+        public void cooltext() //JuSt FoR fUn
+        {
+            while (true)
+            {
+                ByMuphlonLabel.ForeColor = Color.Blue;
+                Thread.Sleep(300);
+                ByMuphlonLabel.ForeColor = Color.Red;
+                Thread.Sleep(300);
+                ByMuphlonLabel.ForeColor = Color.Green;
+                Thread.Sleep(300);
+                ByMuphlonLabel.ForeColor = Color.DarkGoldenrod;
+                Thread.Sleep(300);
+                ByMuphlonLabel.ForeColor = Color.Orange;
+                Thread.Sleep(300);
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread th2 = new Thread(cooltext);
+            th2.Start();
+            if (File.Exists("path.txt"))
+            {
+                location = File.ReadAllText("path.txt");
+                label1.Text = location;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+    }
+}
